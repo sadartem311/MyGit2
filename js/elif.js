569ccbd13a5fc4867b8f0b66fce433eb9b0ca92a
Sadchikov Artem <sadrosgward@yandex.ru>
2020-09-08 19:29:00 +0300

    My first project: if else for prompt object

---
+++ js/elif.js
@@ -0,0 +1,35 @@
+"use strict"
+const numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели","");
+
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+for(let i=0;i<2;i++){
+    const endFilm=prompt("Один из последнх просмотренных фильмов: ",""),
+          star=prompt("На сколько оцените его?","");
+
+    if (endFilm != null && star != null && endFilm != "" && star != "" && endFilm.length<50){
+        
+        personalMovieBD.movies[endFilm]=star;
+        console.log("done");
+    } else{
+        console.log("no syntaxis");
+        i--;
+    }
+}
+
+if (personalMovieBD.count<10){
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieBD.count>10 && personalMovieBD.count<30){
+    console.log("Вы классический зритель");
+} else if (personalMovieBD.count>30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+console.log(personalMovieBD);
---
+++ js/func.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let a=5;
+
+function showThisFunction(){
+    let a=33;
+    console.log(a);
+}
+
+showThisFunction();
+
+console.log(a);
